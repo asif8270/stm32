041b85b40d9872cca5f8ec0bdf8942ca067ed64c
DESKTOP-EBIGPDG\Asif <asifmehmood.8270@gmail.com>
2024-09-24 12:31:44 +0500

    Environment setup for I2C driver

--- 8_I2C_LIS302DL/Src/main.c
+++ 8_I2C_LIS302DL/Src/main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "stm32f4xx.h"
-#include "uart.h"
 
 int main(void)
 {
-	uart2_tx_init();
+
 	while(1)
 	{
-		printf("Assalam o alaikum from STM32F4.....\r\n");
-		for (int i = 0; i < 1000000; ++i) {}
+
 	}
 }

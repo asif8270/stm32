0a47b3131c37509d76f38ae9906e350d7a7b2898
DESKTOP-EBIGPDG\Asif <asifmehmood.8270@gmail.com>
2024-10-03 12:43:59 +0500

    I2C driver development

---
+++ 8_I2C/Inc/i2c.h
@@ -0,0 +1,15 @@
+/*
+ * i2c.h
+ *
+ *  Created on: Sep 24, 2024
+ *      Author: Asif
+ */
+
+#ifndef I2C_H_
+#define I2C_H_
+
+#define GPIOB_EN		(1U<<2)
+#define I2C1_EN			(1U<<21)
+
+
+#endif /* I2C_H_ */
